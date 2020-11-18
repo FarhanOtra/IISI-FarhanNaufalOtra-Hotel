13297ef07f41e20659e77bd24848ed77382296d9
FarhanOtra <otra.farhan@gmail.com>
2020-11-18 20:42:14 +0700

    First

---
+++ hotel_bkt/admin/inc/header.php
@@ -0,0 +1,12 @@
+
+  <header class="header black-bg">  
+	  <div class="sidebar-toggle-box" style="color: white">
+		  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation">
+		  </div>
+		</div>         
+    <a class="logo"><p style="color: white">WebGIS Hotel ( 22-1811522026 Farhan Naufal Otra )</p></a>
+      <ul class="nav pull-right top-menu" style="color: white">
+        <a href="act/logout.php" class="logo" style="font-size:14px; color: white"><i class="fa fa-sign-in"></i>
+        <b>Logout</b></a>
+      </ul>
+  </header>        
