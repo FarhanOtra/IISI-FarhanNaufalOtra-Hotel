b395fd7de79872cfbc430c71330424a51d2556f7
FarhanOtra <58206514+FarhanOtra@users.noreply.github.com>
2020-11-18 22:01:07 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "localhost";
-	$user = "postgres";
-	$pass = "farhan5760";
+	$host = "ec2-3-213-106-122.compute-1.amazonaws.com";
+	$user = "caezpqzytoslhz";
+	$pass = "577563425eb029010b2ce4229390f0811d09920e53d05892286e967df896b935";
 	$port = "5432";
-	$dbname = "bkt_tourism";
+	$dbname = "dc0e5b8t4qql4d";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
